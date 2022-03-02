0f05fbce6f96bd2ff7c54605fcc681833c92e67b
xmirz <linearattack0@gmail.com>
2022-03-02 19:25:26 +0700

    init getx pattern

---
+++ lib/core/values/config.dart
@@ -0,0 +1,8 @@
+class Config {
+  Config._();
+
+  static const baseUrl = 'https://api.themoviedb.org/3';
+  static const apiKey = 'api_key=0af06c7e624f58a59c11c4e7cf81366d';
+  static const movieUrl = baseUrl + '/movie';
+  static const tvUrl = baseUrl + '/tv';
+}
